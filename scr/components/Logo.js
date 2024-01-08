726aefef806aaaef27224da937781012627b812f
tea-eclair <148536484+tea-eclair@users.noreply.github.com>
2024-01-08 17:31:09 +0500

    Create Logo.js

---
+++ scr/components/Logo.js
@@ -0,0 +1,13 @@
+// Logo.js
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      {/* Add your college logo image or content here */}
+      <img src="path/to/your/rvtkk.png" alt="College Logo" />
+    </div>
+  );
+};
+
+export default Logo;
